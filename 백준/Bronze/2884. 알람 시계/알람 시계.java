a35dbefd53f230959bba100623f1d95ee94af834
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-18 16:45:27 +0900

    [Bronze III] Title: 알람 시계, Time: 236 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int subMinute = 45;
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        if (minute < subMinute) {
+            hour--;
+            if (hour < 0) {
+                hour = 24 - 1;
+            }
+            minute = minute - subMinute + 60;
+        } else {
+            minute = minute - subMinute;
+        }
+
+        System.out.println(hour + " " + minute);
+    }
+}
