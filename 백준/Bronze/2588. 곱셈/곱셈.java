b59e3bfbc50cab2b9192d519d009397cbc0498f9
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-18 16:54:51 +0900

    [Bronze III] Title: 곱셈, Time: 252 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, e, f;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a * (b % 10);
+        d = a * ((b / 10) % 10);
+        e = a * (b / 100);
+        f = c + d * 10 + e * 100;
+        System.out.println(c + "\n" + d + "\n" + e + "\n" + f);
+    }
+}
