db4a219f988b77f09ee3951feb6daee3d0e3608b
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-18 16:49:43 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = 10;
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int newNum = Integer.parseInt(br.readLine()) % 42;
+            boolean isExist = list.contains(newNum);
+            if (!isExist) {
+                count++;
+            }
+            list.add(newNum);
+        }
+        bw.write(Integer.toString(count));
+        bw.flush();
+        bw.close();
+    }
+}
