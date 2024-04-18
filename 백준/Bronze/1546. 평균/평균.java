5e5fcde08bac14c70e65f59453890a1695ba537e
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-18 16:49:20 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int max = 0;
+        double sum = 0;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        double[] newArr = new double[N];
+        
+        st = new StringTokenizer(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] > max) max = arr[i];
+        }
+        for (int i = 0; i < N; i++) {
+            newArr[i] = arr[i] / (double) max * 100;
+            sum += newArr[i];
+        }
+        bw.write(Double.toString(sum / N));
+        bw.flush();
+        bw.close();
+    }
+}
