0f1b3807b0301767bf31f4a6e4db3b750a48acdc
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-18 16:50:50 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[9];
+        int max = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i + 1;
+            }
+        }
+        bw.write(((Integer) max).toString());
+        bw.newLine();
+        bw.write(((Integer) maxIndex).toString());
+        bw.flush();
+        bw.close();
+    }
+}
