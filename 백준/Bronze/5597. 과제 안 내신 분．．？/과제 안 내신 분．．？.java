c38cf66810a80e5bf6e3bf3da22f27588d869dae
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-18 16:49:57 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str;
+        int students = 30, num;
+        int[] arr = new int[students];
+        
+        while ((str = br.readLine()) != null && !str.isEmpty()) {
+            num = Integer.parseInt(str);
+            arr[num - 1] = num;
+        }
+
+        for (int i = 0; i < students; i++) {
+            if (arr[i] == 0) {
+                bw.write(Integer.toString(i + 1));
+                bw.newLine();
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
