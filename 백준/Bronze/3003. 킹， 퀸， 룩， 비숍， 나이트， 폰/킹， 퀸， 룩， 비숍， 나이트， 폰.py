8b892b55bd57ca8206340b9c5b3729c5f3fe15a3
helloItsUniverse <123242520+helloItsUniverse@users.noreply.github.com>
2024-04-22 10:19:01 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,7 @@
+data = list(map(int, input().split()))
+ref = [1, 1, 2, 2, 2, 8]
+# print(data)
+for i in range(len(data)):
+    print(f"{ref[i] - data[i]}", end="")
+    if i != len(data) - 1:
+        print(" ", end="")
